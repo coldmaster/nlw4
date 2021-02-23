c5e44f26457b9d72ad4c00c76d12c6828163dd88
glauber.castro <glauber.castro@insa.gov.br>
2021-02-23 15:19:03 -0300

    codigos do segundo dia

---
+++ aulas/moveit-next/src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import {CompleteChallenges} from "../components/CompleteChallenges"
+
+import styles from '../styles/pages/Home.module.css'
+import { Countdown } from "../components/Countdown";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+          <ExperienceBar />
+          <section>
+            <div>
+               <Profile />
+               <CompleteChallenges />
+               <Countdown />
+            </div>
+            <div>
+
+            </div>
+          </section>
+    </div>
+  )
+}
