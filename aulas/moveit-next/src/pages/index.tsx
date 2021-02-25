8cc439e36706a65d297334994f6f1e2380964237
worldmaster <glaubercold53@gmail.com>
2021-02-24 21:34:57 -0300

    novos codigos

--- aulas/moveit-next/src/pages/index.tsx
+++ aulas/moveit-next/src/pages/index.tsx
@@ -4,11 +4,15 @@ import {CompleteChallenges} from "../components/CompleteChallenges"
 
 import styles from '../styles/pages/Home.module.css'
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+        
+
           <ExperienceBar />
+
           <section>
             <div>
                <Profile />
@@ -16,7 +20,7 @@ export default function Home() {
                <Countdown />
             </div>
             <div>
-
+               <ChallengeBox />
             </div>
           </section>
     </div>
